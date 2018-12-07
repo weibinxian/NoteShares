d93cc9fb0a94071dd8e3b228634a133ddfecca77
weibinxian <37277542+weibinxian@users.noreply.github.com>
2018-12-07 01:23:15 -0500

    add comments

--- client/src/components/commentComponent/comments.js
+++ client/src/components/commentComponent/comments.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react'
-
+import {newcom} from '../userFunctions'
 
 class Comment extends Component{
-    render() {
-        return (
-            <div>Comment</div>
+    constructor(props){
+        super(props)
+
+        this.state = { 
+            body: '',
+        }
+    }
+    onChange(e) {
+        let body = this.state.body;
+        body = e.target.value;
+        this.setState({body});
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        //post fetch
+        newcom(this.state.body);
+
+    }
+    render(){
+        return(
+            <form onSubmit={(e) => this.onSubmit(e)}>
+            <div className="form">
+            <div className="form-row align-items-center">
+                <div className="col-auto">
+                    <input className="form-control"
+                           onChange = { (e) => this.onChange(e) }
+                           required></input>
+                </div>
+                <div className="col-auto">
+                    <button className="btn btn-primary"
+                            type="submit">Comment
+                    </button>
+                </div>
+            </div>
+            </div>
+            </form>
         );
     }
 }
